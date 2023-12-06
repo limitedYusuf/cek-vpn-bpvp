d6347a2dab8a6271af7e650de784146a7960c16e
idiotRun <doflamingod642@gmail.com>
2023-12-07 00:48:42 +0800

    rollback to ip api

--- api/proxy.php
+++ api/proxy.php
@@ -13,14 +13,14 @@ if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
    $clientIP = $_SERVER['REMOTE_ADDR'];
 }
 
-$ipInfo = json_decode(file_get_contents('https://ipinfo.io/json?token=66f1654f0055ae&nocache=' . time()));
+$ipInfo = json_decode(file_get_contents("https://ipinfo.io/$clientIP?token=66f1654f0055ae&nocache=" . time()));
 $ipAddress = $ipInfo->ip;
 
-$proxyCheckResponse = file_get_contents("https://proxycheck.io/v2/$clientIP?key=public-35w8n2-878lub-12468p&asn=1&timestamp=" . time());
+$proxyCheckResponse = file_get_contents("https://proxycheck.io/v2/$ipAddress?key=public-35w8n2-878lub-12468p&asn=1&timestamp=" . time());
 $proxyCheckData = json_decode($proxyCheckResponse, true);
 
-if (isset($proxyCheckData[$clientIP]['proxy']) && $proxyCheckData[$clientIP]['proxy'] == 'yes') {
-   echo json_encode(['isUsingVPN' => true, 'geo' => $ipInfo, 'info' => $proxyCheckData[$clientIP]]);
+if (isset($proxyCheckData[$ipAddress]['proxy']) && $proxyCheckData[$ipAddress]['proxy'] == 'yes') {
+   echo json_encode(['isUsingVPN' => true, 'geo' => $ipInfo, 'info' => $proxyCheckData[$ipAddress]]);
 } else {
-   echo json_encode(['isUsingVPN' => false, 'geo' => $ipInfo, 'info' => $proxyCheckData[$clientIP]]);
+   echo json_encode(['isUsingVPN' => false, 'geo' => $ipInfo, 'info' => $proxyCheckData[$ipAddress]]);
 }
